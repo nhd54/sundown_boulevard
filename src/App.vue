f385c8f9f148498c324f0038592535f8f3e21dd9
nhd54 <nicklasdegnebolig@gmail.com>
2021-02-08 12:00:09 +0100

    first commit - with header & primary colors

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <main>
+   <nav class="primary-background-color">
+    <div class="nav-wrapper container">
+      <a href="#" class="brand-logo left">
+         <img src="images/beach.png"/>
+      </a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="sass.html">Resturanter</a></li>
+        <li><a href="badges.html">Produkter</a></li>
+        <li><a href="collapsible.html">Nyhedsbrev</a></li>
+        <li><a href="collapsible.html">Kontakt</a></li>
+      </ul>
+    </div>
+  </nav>
+  <router-view/>
+  </main>
+</template>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+ ul, ul li {
+   margin-left: 50px;
+ }
+
+ 
+  nav .brand-logo {
+    position:inherit;
+
+}
+
+.primary-background-color{
+   background-color:#BA2329;
+}
+
+.primary-text-color {
+   color:#BA2329;  
+}
+
+.secondary-background-color {
+  background-color: #007DDB
+}
+.secondary-text-color {
+  color: #007DDB
+}
+</style>
